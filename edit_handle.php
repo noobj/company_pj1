1283b1a9a1adc8b8a911a782c251d23cd33ae4c9
jim <jingfu99@gmail.com>
2014-06-17 17:55:19 +0800

    Adding a edit message function

--- edit_handle.php
+++ edit_handle.php
@@ -1,36 +1,36 @@
 <?php
-      try{
+try {
     $pdo =new PDO('mysql:host=localhost;dbname=test','root','12345678');
-    }catch(PDOException $e)
-    {
-        echo "conld not connect";
-    }
+} catch(PDOException $e)
+{
+  echo "conld not connect";
+}
 
-    if(isset($_POST['message']))
-    {
-        $sql = "UPDATE message SET content = :content WHERE id = :id;";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':content', $_POST['message']);
-        $stmt->bindValue(':id', $_POST['id']);
-        $stmt->execute();
-        echo "success";
-    }    
+if(isset($_POST['message']))
+{
+    $sql = "UPDATE message SET content = :content WHERE id = :id;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':content', $_POST['message']);
+    $stmt->bindValue(':id', $_POST['id']);
+    $stmt->execute();
+    echo "success";
+}
 
- 
-    
-    if(isset($_POST['id']))
-    {
-        $sql = "SELECT * FROM message WHERE id = :id";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':id', $_POST['id']);
-        $stmt->execute();
-        $row = $stmt->fetch();
-    }              
-    echo "<form action=". $_SERVER['PHP_SELF']." method='post'>";
-    echo "<input type='textarea' name='message' value=".$row['content']." />";
-    echo "<input type='hidden' name='id' value=".$_POST['id']."/>";
+
+if(isset($_POST['id']))
+{
+    $sql = "SELECT * FROM message WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':id', $_POST['id']);
+    $stmt->execute();
+    $row = $stmt->fetch();
+}
+echo "<form action=". $_SERVER['PHP_SELF']." method='post'>";
+echo "<input type='textarea' name='message' value=".$row['content']." />";
+echo "<input type='hidden' name='id' value=".$_POST['id']."/>";
 ?>
 <input type="submit" value="submit" />
 </form>
 
 <a href="main.php">main</a>
+
