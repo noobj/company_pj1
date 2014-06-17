d2c2d3981c539f9e779c21b8e023b145d59ba093
jim <jingfu99@gmail.com>
2014-06-17 17:52:16 +0800

    Fixing program coding format and variable to fit in with PSR style

--- delete.php
+++ delete.php
@@ -1,38 +1,31 @@
 <?php
-    try{
-    $pdo =new PDO('mysql:host=localhost;dbname=test','root','12345678');
-    }catch(PDOException $e)
-    {
-        echo "conld not connect";
-    }
-    
-    if(isset($_POST['id']))
-    {
-        $sql = "DELETE FROM message WHERE id = :id";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':id', $_POST['id']);
-        $stmt->execute();
-    }
-        
-        
-        
+try {
+$pdo = new PDO('mysql:host=localhost;dbname=test','root','12345678');
+} catch(PDOException $e) {
+    echo "conld not connect";
+}
+
+if (isset($_POST['id'])) {
+    $sql = "DELETE FROM message WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':id', $_POST['id']);
+    $stmt->execute();
+}
 ?>
-
-
 <form name="form" action=<?php echo $_SERVER['PHP_SELF'];?> method="post">
     Select ID to delete:
+
 <?php
-
-
-    $result = $pdo->query('select * from message');
+$result = $pdo->query('select * from message');
 ?>
 <select name="id">
-    <?php
-        while($row = $result->fetch())
-        {
-            echo "<option value=".$row['id'].">".$row['id']."</option>";
-        }
-    ?>
+
+<?php
+while($row = $result->fetch()) {
+    echo "<option value=".$row['id'].">".$row['id']."</option>";
+}
+?>
+
 </select>
 <input type="submit" value="submit" />
 </form>
--- handle.php
+++ handle.php
@@ -1,15 +1,12 @@
 <?php
-    try{
-    $pdo =new PDO('mysql:host=localhost;dbname=test','root','12345678');
-    }catch(PDOException $e)
-    {
-        echo "conld not connect";
-    }
-    
-    $sql = "insert into message (content,user) values (:content,:user)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(':content' => $_POST['message'],':user' => $_POST['name']));
-    
+try{
+$pdo =new PDO('mysql:host=localhost;dbname=test','root','12345678');
+} catch(PDOException $e) {
+    echo "conld not connect";
+}
+$sql = "insert into message (content, user) values (:content, :user)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(array(':content' => $_POST['message'],':user' => $_POST['name']));
 ?>
 
 <meta http-equiv="refresh" content="0;url='main.php'">
